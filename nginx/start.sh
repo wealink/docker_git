43606bdfd42667a74edf008f6fb39647c069813f
995977154@qq.com <995977154@qq.com>
2017-12-14 16:22:51 +0800

    nginx-v1

---
+++ nginx/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run -d -p 80:80 \
+              -p 443:443 \
+	      -v `pwd`/conf:/opt/nginx/conf:rw \
+	      -v `pwd`/html:/opt/nginx/html:rw \
+	      -v `pwd`/logs:/opt/nginx/logs:rw \
+	      --name test_nginx \
+	      zhaohongbo/nginx1.10
