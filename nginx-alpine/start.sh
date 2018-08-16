b74cd39c6a5c1b22f6c792dc20677f8276152513
zhb <995977154@qq.com>
2018-08-16 23:59:01 +0800

    network host

--- nginx-alpine/start.sh
+++ nginx-alpine/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-docker run -d -p 80:80 \
-              -p 443:443 \
-	      -v `pwd`/conf/nginx.conf:/opt/nginx/conf/nginx.conf:rw \
+docker run -d -v `pwd`/conf/nginx.conf:/opt/nginx/conf/nginx.conf:rw \
 	      -v `pwd`/conf/conf.d:/opt/nginx/conf/conf.d:rw \
 	      -v `pwd`/logs:/opt/nginx/logs:rw \
 	      -v `pwd`/web:/opt/web:rw \
+	      --network host \
+	      --restart always \
 	      --name nginx \
 	      zhaohongbo/nginx:1.12
